c79b75e00458c0e4ac4f31a68c99245fc8776265
Sheepheerd <sheepheerd@novastar>
2024-12-04 02:48:03 -0500

    ft

--- config/sets/set.nix
+++ config/sets/set.nix
@@ -47,8 +47,8 @@
 
       # Enable persistent undo history
       swapfile = false;
-      backup = false;
-      undofile = true;
+      backup = true;
+      undofile = false;
 
       # Enable 24-bit colors
       termguicolors = true;
@@ -119,21 +119,21 @@
       local opt = vim.opt
       local g = vim.g
       local o = vim.o
-      --   -- Neovide
-      -- if g.neovide then
-      --   g.neovide_fullscreen = false
-      --   g.neovide_hide_mouse_when_typing = false
-      --   g.neovide_refresh_rate = 165
-      --   g.neovide_cursor_vfx_mode = "ripple"
-      --   g.neovide_cursor_animate_command_line = true
-      --   g.neovide_cursor_animate_in_insert_mode = true
-      --   g.neovide_cursor_vfx_particle_lifetime = 5.0
-      --   g.neovide_cursor_vfx_particle_density = 14.0
-      --   g.neovide_cursor_vfx_particle_speed = 12.0
-      --   g.neovide_transparency = 0.8
-      --
-      --   -- Neovide Fonts
-      --   o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
+        -- Neovide
+      if g.neovide then
+        g.neovide_fullscreen = false
+        g.neovide_hide_mouse_when_typing = false
+        g.neovide_refresh_rate = 165
+        g.neovide_cursor_vfx_mode = "ripple"
+        g.neovide_cursor_animate_command_line = true
+        g.neovide_cursor_animate_in_insert_mode = true
+        g.neovide_cursor_vfx_particle_lifetime = 5.0
+        g.neovide_cursor_vfx_particle_density = 14.0
+        g.neovide_cursor_vfx_particle_speed = 12.0
+        g.neovide_transparency = 0.8
+
+        -- Neovide Fonts
+        o.guifont = "JetBrainsMono Nerd Font:h14:Medium:i"
       end
     '';
   };
