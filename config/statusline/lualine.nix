fdb20f97ef829a7d445668bec8599450682525e6
Sheepheerd <sheepheerd@novastar>
2024-12-04 02:36:40 -0500

    Trying to rm notifications

--- config/statusline/lualine.nix
+++ config/statusline/lualine.nix
@@ -1,8 +1,5 @@
-{ lib, config, ... }:
-{
-  options = {
-    lualine.enable = lib.mkEnableOption "Enable lualine module";
-  };
+{ lib, config, ... }: {
+  options = { lualine.enable = lib.mkEnableOption "Enable lualine module"; };
   config = lib.mkIf config.lualine.enable {
     plugins.lualine = {
       enable = true;
@@ -22,12 +19,7 @@
         };
         sections = {
           lualine_a = [ "mode" ];
-          lualine_b = [
-            "branch"
-            ""
-            "diff"
-            "diagnostics"
-          ];
+          lualine_b = [ "branch" "" "diff" "diagnostics" ];
           lualine_c = [ "filename" ];
           lualine_x = [ "filetype" ];
           lualine_y = [ "progress" ];
@@ -100,12 +92,12 @@
        		local function navic_is_available()
        			return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
        		end
-       		local cmd_mode = function()
-       			return require("noice").api.status.mode.get()
-       		end
-       		local show_mode = function()
-       			return package.loaded["noice"] and require("noice").api.status.mode.has() or ""
-       		end
+       		-- local cmd_mode = function()
+       		-- 	return require("noice").api.status.mode.get()
+       		-- end
+       		-- local show_mode = function()
+       		-- 	return package.loaded["noice"] and require("noice").api.status.mode.has() or ""
+       		-- end
         end
     '';
   };
