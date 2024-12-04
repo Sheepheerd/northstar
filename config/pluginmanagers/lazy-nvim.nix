9cb15d0f73dce9959eba04502320a657dd7b6dbd
Sheepheerd <sheepheerd@novastar>
2024-12-04 13:06:15 -0500

    jdtls

--- config/pluginmanagers/lazy-nvim.nix
+++ config/pluginmanagers/lazy-nvim.nix
@@ -1,15 +1,19 @@
-{
-  lib,
-  config,
-  ...
-}:
-{
+{ lib, config, ... }: {
+  extraLuaConfig = ''
+    require("lazy").setup({
+        {
+          "supermaven-inc/supermaven-nvim",
+          config = function()
+            require("supermaven-nvim").setup({})
+          end,
+        },
+    }, {})
+
+
+  '';
   options = {
     lazy-nvim.enable = lib.mkEnableOption "Enable lazy-nvim module";
   };
-  config = lib.mkIf config.lazy-nvim.enable {
-    plugins.lazy = {
-      enable = true;
-    };
-  };
+  config =
+    lib.mkIf config.lazy-nvim.enable { plugins.lazy = { enable = true; }; };
 }
