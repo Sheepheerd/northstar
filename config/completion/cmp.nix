2a21d7f233ce19446f32e626539acda04f73f34a
Sheepheerd <sheepheerd@novastar>
2024-12-04 16:32:35 -0500

    codeium

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -15,7 +15,7 @@
           mapping = {
             "<Tab>" = null;
 
-            "<S-Tab>" = null;
+            #"<S-Tab>" = null;
 
             "<C-j>" = "cmp.mapping.select_next_item()";
             "<C-k>" = "cmp.mapping.select_prev_item()";
