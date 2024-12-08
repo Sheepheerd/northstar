b79adb561fc2b898703b299a9187153d534b4c85
Sheepheerd <sheepheerd@novastar>
2024-12-07 23:54:22 -0500

    Copilot

--- config/completion/default.nix
+++ config/completion/default.nix
@@ -7,6 +7,7 @@
   config = lib.mkIf config.completion.enable {
     cmp.enable = lib.mkDefault true;
     lspkind.enable = lib.mkDefault true;
-    codeium-nvim.enable = lib.mkDefault true;
+    codeium-nvim.enable = lib.mkDefault false;
+    copilot.enable = lib.mkDefault true;
   };
 }
