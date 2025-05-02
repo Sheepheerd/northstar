319167af1820b991edde8f95cc8d6bebb4db1eb3
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-05-02 05:03:44 -0400

    working on rust

--- config/plugins/languages/rustaceanvim.nix
+++ config/plugins/languages/rustaceanvim.nix
@@ -4,16 +4,16 @@
     enable = true;
     settings = {
       tools.float_win_config.border = "rounded";
-      # server = {
-      #   cmd = [ "rustup" "run" "nightly" "rust-analyzer" ];
-      #   default_settings = {
-      #     rust-analyzer = {
-      #       check = { command = "clippy"; };
-      #       inlayHints = { lifetimeElisionHints = { enable = "always"; }; };
-      #     };
-      #   };
-      #   standalone = false;
-      # };
+      server = {
+        cmd = [ "rustup" "run" "nightly" "rust-analyzer" ];
+        default_settings = {
+          rust-analyzer = {
+            check = { command = "clippy"; };
+            inlayHints = { lifetimeElisionHints = { enable = "always"; }; };
+          };
+        };
+        standalone = false;
+      };
     };
   };
 
